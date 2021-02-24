20eeba5480936e38313b884c6a9eeff396ba215a
Marat <m.bulekbayev@kazdream.kz>
2021-02-24 20:29:00 +0600

    Uploading third task's solution

--- src/main/java/javafundamentals/MainTask.java
+++ src/main/java/javafundamentals/MainTask.java
@@ -1,4 +1,15 @@
 package javafundamentals;
 
 public class MainTask {
+
+    public static void main(String[] args) {
+        try {
+            for (String iterator : args) {
+                System.out.println(Integer.parseInt(iterator));
+                System.out.print(Integer.parseInt(iterator));
+            }
+        } catch (NumberFormatException nfe) {
+            System.out.println("\n" + nfe);
+        }
+    }
 }
