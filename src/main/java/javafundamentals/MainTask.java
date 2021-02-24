6521e9848bcfe803194d1d7fe5a88dec615675b3
Marat <m.bulekbayev@kazdream.kz>
2021-02-24 21:05:34 +0600

    Uploading forth task's solution

--- src/main/java/javafundamentals/MainTask.java
+++ src/main/java/javafundamentals/MainTask.java
@@ -1,4 +1,22 @@
 package javafundamentals;
 
 public class MainTask {
+
+    public static void main(String[] args) {
+        try {
+            int sumResult = Integer.parseInt(args[0]);
+            int multiplyResult = Integer.parseInt(args[0]);
+
+            for (int i = 1; i < args.length; i++) {
+                sumResult += Integer.parseInt(args[i]);
+                multiplyResult *= Integer.parseInt(args[i]);
+            }
+
+            System.out.println("Addition result: " + sumResult);
+            System.out.println("Multiplication result: " + multiplyResult);
+
+        } catch (NumberFormatException nfe) {
+            System.out.println("Argument must be integer! " + nfe.getLocalizedMessage());
+        }
+    }
 }
