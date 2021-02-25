f2118e975a5ba37c23468a5f0c21689696ea4922
Marat <m.bulekbayev@kazdream.kz>
2021-02-25 10:42:02 +0600

    Fixing mistakes

--- src/main/java/javafundamentals/MainTask.java
+++ src/main/java/javafundamentals/MainTask.java
@@ -5,11 +5,16 @@ public class MainTask {
     public static void main(String[] args) {
         try {
             for (String iterator : args) {
+                System.out.print(Integer.parseInt(iterator) + " ");
+            }
+
+            System.out.print("\n");
+
+            for (String iterator : args) {
                 System.out.println(Integer.parseInt(iterator));
-                System.out.print(Integer.parseInt(iterator));
             }
         } catch (NumberFormatException nfe) {
-            System.out.println("\nArgument must be integer! " + nfe.getLocalizedMessage());
+            System.out.println("Argument must be integer! " + nfe.getLocalizedMessage());
         }
     }
 }
